ae334ab89da44dde1a8fc8e355447e11e1f5898a
Ales Holecek <ales@Aless-MacBook-Pro.local>
2018-05-02 00:11:52 -0700

    Working File List

--- Server/uploads/server.js
+++ Server/uploads/server.js
@@ -1,3 +1,19 @@
+/* 
+var http=require('http');
+var server=http.createServer((request,response)=>{
+    response.writeHead(200, {
+        "Conetnt-Type" : "text/plain"
+    });
+    response.end("Hello World\n");
+});
+console.log("listening...")
+server.listen(8080);
+*/
+
+/*
+ * NodeJS code.
+ */
+
 // Required modules.
 var express = require('express');
 var http = require('http');
@@ -9,46 +25,36 @@ var app = express();
 var port = 8080;
 
 app.get('/', (req, res) => {
-    res.status(200).sendFile(__dirname + '/index.html');
+    res.send('Go to upload');
 });
 
 app.get('/upload', (req, res) => {
-    const folder = __dirname + '/uploads/';
-    var fileList = [];
-    fs.readdirSync(folder).forEach(file => {
-        var fileObj = { 'name': file, 'url': '/uploads/' + file };
-        fileList.push(fileObj);
-    });
-    res.status(200).json(fileList);
-});
-
-app.get('/uploads/:file', (req, res) => {
-    var fp = path.join(__dirname, '/uploads/', req.params.file);
-    console.log(fp);
-    res.status(200).sendFile(fp);
+    res.status(200).sendFile(__dirname + '/index.html');
 });
 
 // Upload route.
-app.post('/upload', function (req, res) {
+app.post('/upload', function(req, res) {
     var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
+    form.parse(req, function(err, fields, files) {
         // file is the name of the <input> field of type file
+
         var old_path = files.file.path;
         var file_size = files.file.size;
         var file_ext = files.file.name.split('.').pop();
         var index = old_path.lastIndexOf('/') + 1;
         var file_name = old_path.substr(index);
+//         var new_path = path.join(process.env.PWD, '/uploads/', file_name + '.' + file_ext);
         var new_path = path.join(process.env.PWD, __dirname + '/uploads/', files.file.name);
 
-        fs.readFile(old_path, function (err, data) {
-            fs.writeFile(new_path, data, function (err) {
-                fs.unlink(old_path, function (err) {
+        fs.readFile(old_path, function(err, data) {
+            fs.writeFile(new_path, data, function(err) {
+                fs.unlink(old_path, function(err) {
                     if (err) {
                         res.status(500);
-                        res.json({ 'success': false });
+                        res.json({'success': false});
                     } else {
                         res.status(200);
-                        res.json({ 'success': true });
+                        res.json({'success': true});
                     }
                 });
             });
