7e378ce033014ec3d0f0ac56dc3615145ab22bf9
Ales Holecek <ales@Aless-MacBook-Pro.local>
2018-05-01 21:11:05 -0700

    Improving file list

--- Server/server.js
+++ Server/server.js
@@ -22,18 +22,29 @@ var fs = require('fs');
 var path = require('path');
 var app = express();
 
+var port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Go to upload');
+});
+
+app.get('/upload', (req, res) => {
+    res.status(200).sendFile(__dirname + '/index.html');
+});
 
 // Upload route.
 app.post('/upload', function(req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files) {
         // file is the name of the <input> field of type file
-        var old_path = files.file.path,
-            file_size = files.file.size,
-            file_ext = files.file.name.split('.').pop(),
-            index = old_path.lastIndexOf('/') + 1,
-            file_name = old_path.substr(index),
-            new_path = path.join(process.env.PWD, '/uploads/', file_name + '.' + file_ext);
+
+        var old_path = files.file.path;
+        var file_size = files.file.size;
+        var file_ext = files.file.name.split('.').pop();
+        var index = old_path.lastIndexOf('/') + 1;
+        var file_name = old_path.substr(index);
+//         var new_path = path.join(process.env.PWD, '/uploads/', file_name + '.' + file_ext);
+        var new_path = path.join(process.env.PWD, __dirname + '/uploads/', files.file.name);
 
         fs.readFile(old_path, function(err, data) {
             fs.writeFile(new_path, data, function(err) {
@@ -50,3 +61,5 @@ app.post('/upload', function(req, res) {
         });
     });
 });
+
+app.listen(port, () => console.log('Listening on port: ' + port));
